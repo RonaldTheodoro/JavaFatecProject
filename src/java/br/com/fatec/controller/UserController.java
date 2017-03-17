b4248e775c2d1839b64db1fc31806f85ee91a60f
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-03-17 00:09:38 -0300

    add ColaboratorController

--- src/java/br/com/fatec/controller/UserController.java
+++ src/java/br/com/fatec/controller/UserController.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.fatec.controller;
+import br.com.fatec.bean.User;
+import br.com.fatec.model.UserDao;
 
-/**
- *
- * @author ronaldtheodoro
- */
+
+
 public class UserController {
     
 }
