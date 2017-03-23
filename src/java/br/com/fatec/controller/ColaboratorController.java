c4fda6de76a4ed960aa4e04adeaa05cb0bb2538d
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-03-23 00:37:52 -0300

    add ColaboratorController constructor

--- src/java/br/com/fatec/controller/ColaboratorController.java
+++ src/java/br/com/fatec/controller/ColaboratorController.java
@@ -4,8 +4,14 @@ import br.com.fatec.bean.Colaborator;
         
 
 public class ColaboratorController {
+    private Colaborator colaborator;
     
-    public Colaborator insertColaborator(Colaborator colaborator) {
+    public ColaboratorController(Colaborator colaborator) {
+        this.colaborator = colaborator;
+                
+    }
+    
+    public Colaborator insertColaborator() {
         colaborator.setType("Test");
         return colaborator;
     }
