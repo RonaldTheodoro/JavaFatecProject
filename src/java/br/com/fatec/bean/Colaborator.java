0e8b5df200c3486bd49c626e970a10b59c689665
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-03-23 00:38:15 -0300

    add Colaborator constructor

--- src/java/br/com/fatec/bean/Colaborator.java
+++ src/java/br/com/fatec/bean/Colaborator.java
@@ -2,8 +2,22 @@ package br.com.fatec.bean;
 
 
 public class Colaborator extends Person {
+    protected String name;
     protected String type;
+    
+    public Colaborator(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+    
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public String getType() {
         return type;
     }
