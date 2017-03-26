c1a5e9fff53a3e374aefeff4f8b2576826fcfbbe
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-03-25 22:45:34 -0300

    add ClassNotFoundException in try catch

--- src/java/br/com/fatec/util/ConnectionDatabase.java
+++ src/java/br/com/fatec/util/ConnectionDatabase.java
@@ -5,11 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDatabase {
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
+    public Connection getConnection() {
         try {
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection("jdbc:sqlite:db.sqlite3");
         } catch (SQLException error) {
+            throw new RuntimeException(error);
+        } catch (ClassNotFoundException error) {
             throw new RuntimeException(error);
         }
     }
