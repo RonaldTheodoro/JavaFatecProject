076d75d2071086100328014ed548ebffc999e3b0
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-03-25 22:46:18 -0300

    add prepareStatement method

--- src/java/br/com/fatec/model/UserDao.java
+++ src/java/br/com/fatec/model/UserDao.java
@@ -1,7 +1,5 @@
 package br.com.fatec.model;
 
-import br.com.fatec.bean.User;
-import br.com.fatec.util.ConnectionDatabase;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,9 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.fatec.bean.User;
+import br.com.fatec.util.ConnectionDatabase;
+
 public class UserDao {
     private final Connection connection;
-    private PreparedStatement preparedStatement;
+    private PreparedStatement statement;
     private ResultSet resultSet;
     
     public UserDao() throws SQLException, ClassNotFoundException {
@@ -19,11 +20,12 @@ public class UserDao {
     }
     
     public User search(User user) throws SQLException {
-        String SQL = "SELECT * FROM user WHERE id = ?";
+        String sql = "SELECT * FROM user WHERE id = ?";
         
-        try (PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
-            preparedStatement.setInt(1, user.getId());
-            resultSet = preparedStatement.executeQuery();
+        try {
+            prepareStatement(sql);
+            statement.setInt(1, user.getId());
+            resultSet = statement.executeQuery();
             
             while (resultSet.next()) {
                 user.setId(resultSet.getInt(1));
@@ -32,22 +34,23 @@ public class UserDao {
                 user.setPassword(resultSet.getString(4));
                 user.setStatus(resultSet.getString(5));
                 user.setType(resultSet.getString(6));
-                
             }
+            return user;
+        } catch (SQLException error) {
+            throw new RuntimeException(error);
         }
-        return user;
     }
     
     public List<User> list(User user) throws SQLException {
         
         List<User> usersList = new ArrayList<>();
-        String SQL = "SELECT * FROM user WHERE login like ?";
+        String SQL = "SELECT * FROM user WHERE name like ?";
         
-        preparedStatement = connection.prepareStatement(SQL);
+        prepareStatement(SQL);
         
-        preparedStatement.setString(1, "%" + user.getLogin() + "%");
+        statement.setString(1, "%" + user.getName() + "%");
         
-        resultSet = preparedStatement.executeQuery();
+        resultSet = statement.executeQuery();
         
         while (resultSet.next()) {
             User userData = new User();
@@ -60,59 +63,66 @@ public class UserDao {
             
             usersList.add(userData);
         }
-        
+        resultSet.close();
+        statement.close();
         return usersList;
     }
     
-    public void insert(User user) throws SQLException {
-        String SQL = "INSER INTO user (login, password, status, tipo) values (?, ?, ?, ?, ?)";
+    public User insert(User user) throws SQLException {
+        String SQL = "INSERT INTO user" + 
+            "(login, password, status, tipo) values (?, ?, ?, ?, ?)";
         
-        preparedStatement = connection.prepareStatement(SQL);
+        prepareStatement(SQL);
         
-        preparedStatement.setString(1, user.getLogin());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.setString(3, user.getStatus());
-        preparedStatement.setString(4, user.getType());
+        statement.setString(1, user.getName());
+        statement.setString(2, user.getLogin());
+        statement.setString(3, user.getPassword());
+        statement.setString(4, user.getStatus());
+        statement.setString(5, user.getType());
         
-        preparedStatement.execute();
-        preparedStatement.close();
+        statement.execute();
+        statement.close();
+        return user;
     }
     
-    public void exclude(User user) throws SQLException {
+    public User exclude(User user) throws SQLException {
         String SQL = "DELETE FROM user WHERE id = ?";
-        preparedStatement = connection.prepareStatement(SQL);
+        prepareStatement(SQL);
         
-        preparedStatement.setInt(1, user.getId());
+        statement.setInt(1, user.getId());
         
-        preparedStatement.execute();
-        preparedStatement.close();
+        statement.execute();
+        statement.close();
         connection.close();
+        return user;
     }
     
     public User alter(User user) throws SQLException {
-        String SQL = "UPDATE user SET login = ?, password = ?, status = ?, type = ? WHERE id = ?";
-        preparedStatement = connection.prepareStatement(SQL);
+        String SQL = "UPDATE user SET " + 
+            "name=?, login=?, password=?, status=?, type=? WHERE id=?";
+        prepareStatement(SQL);
         
-        preparedStatement.setString(1, user.getLogin());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.setString(3, user.getStatus());
-        preparedStatement.setString(4, user.getType());
-        preparedStatement.setInt(5, user.getId());
+        statement.setString(1, user.getName());
+        statement.setString(2, user.getLogin());
+        statement.setString(3, user.getPassword());
+        statement.setString(4, user.getStatus());
+        statement.setString(5, user.getType());
+        statement.setInt(6, user.getId());
         
-        preparedStatement.execute();
-        preparedStatement.close();
+        statement.execute();
+        statement.close();
         
         return user;
     }
     
     public User validateLogin(User user) throws SQLException {
-        String SQL = "SELECT * FROM user WHERE id = ?";
-        preparedStatement = connection.prepareStatement(SQL);
+        String SQL = "SELECT * FROM user WHERE login=? AND password=?";
+        prepareStatement(SQL);
         
-        preparedStatement.setString(1, user.getLogin());
-        preparedStatement.setString(2, user.getPassword());
+        statement.setString(1, user.getLogin());
+        statement.setString(2, user.getPassword());
         
-        resultSet = preparedStatement.executeQuery();
+        resultSet = statement.executeQuery();
         
         while (resultSet.next()) {
             user.setId(resultSet.getInt(1));
@@ -122,7 +132,11 @@ public class UserDao {
             user.setStatus(resultSet.getString(5));
             user.setType(resultSet.getString(6));
         }
-        preparedStatement.close();
+        statement.close();
         return user;
+    }
+    
+    private void prepareStatement(String sql) throws SQLException {
+        statement = connection.prepareStatement(sql);
     }
 }
